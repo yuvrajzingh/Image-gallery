ccc29f5998daaff851fdca2a340660da30cdad31
yuvrajzingh <yuvisingh1810@gmail.com>
2023-07-06 12:22:14 +0530

    search placeholder

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -14,10 +14,10 @@ const ImageSearch = ({searchText}) => {
   }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto ">
         <form action="" className="w-full max-w-sm" onSubmit={onSubmit}>
             <div className="flex items-center border-b-2 border-teal-500 py-2">
-                <input onChange={e => setText(e.target.value)} type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder='Search Image Term..' />
+                <input onChange={e => setText(e.target.value)} type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder='Search Image..' />
                 <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type='submit'>
                     Search
                 </button>
